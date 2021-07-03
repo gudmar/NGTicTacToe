a7a6f941658ff7d71fdfd222218ec035a2051491
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-07-03 21:33:34 +0200

    Component for chosing board size implemented. Still needs binding to board

--- src/app/shared/expandable-list/expandable-list.component.ts
+++ src/app/shared/expandable-list/expandable-list.component.ts
@@ -1,13 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, HostListener} from '@angular/core';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+
+
+interface SingleOption {
+  name: string,
+  value: number
+}
 
 @Component({
   selector: 'app-expandable-list',
   templateUrl: './expandable-list.component.html',
   styleUrls: ['./expandable-list.component.css']
 })
-export class ExpandableListComponent implements OnInit {
 
+export class ExpandableListComponent implements OnInit {
+  @Input() listOfOptions: string[] = [];
+  @Input() actualOption: number = 0;
+  isListHidden: boolean = true;
   constructor() { }
+
+  @HostListener('click') toggleListVisibility(){
+    this.isListHidden = this.isListHidden?false:true;
+  }
 
   ngOnInit(): void {
   }
