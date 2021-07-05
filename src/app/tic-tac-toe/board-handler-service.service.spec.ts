bbb24d537662e45f1e44c5a23c32b030fbb43503
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-07-05 23:33:20 +0200

    Tests for figures in row to test test utilities for later tests

--- src/app/tic-tac-toe/board-handler-service.service.spec.ts
+++ src/app/tic-tac-toe/board-handler-service.service.spec.ts
@@ -1,16 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
+// import { BoardHandlerServiceService } from './board-handler-service.service';
+// import { Jasmine_ServiceFunctionTestsFromArrayRunner } from '../shared/tests/jasmine_runTestsFromArray'
+// import { testSuite } from './tests_winnerSearcherTestCases'
+// import { TestFromArrayConfig } from '../app.types'
 
-import { BoardHandlerServiceService } from './board-handler-service.service';
+// describe('BoardHandlerServiceService', () => {
+//   let service: BoardHandlerServiceService;
 
-describe('BoardHandlerServiceService', () => {
-  let service: BoardHandlerServiceService;
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(BoardHandlerServiceService);
+//   });
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BoardHandlerServiceService);
-  });
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+// let testedService = new BoardHandlerServiceService();
+// let testedFunction = 
+
+// let testConfig: TestFromArrayConfig = {
+//   testSuiteName: 'Testing winner on board search',
+//   testedFunction: 
+// }
+
+// let testServiceRunner = new Jasmine_ServiceFunctionTestsFromArrayRunner()
---
+++ src/app/tic-tac-toe/tests_winnerSearcherTestCases.ts
@@ -0,0 +1,49 @@
+let testCase1 = {
+    name: 'Matrix 5x5 find 3 circles in last row',
+    input: [[0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0]],
+    expectedOutput: [[2, 5], [3, 5], [4, 5]]
+};
+let testCase2 = {
+    name: 'Matrix 5x5 find 3 crosses in first row',
+    input: [[0, 0, 2, 2, 2],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]],
+    expectedOutput: [[3, 1], [4, 1], [5, 1]]
+};
+let testCase3 = {
+    name: 'Matrix 5x5 find 3 crosses in first column',
+    input: [[2, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]],
+    expectedOutput: [[1, 1], [1, 2], [1, 3]]
+};
+let testCase4 = {
+    name: 'Matrix 5x5 find 3 circles in third column',
+    input: [[0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0]],
+    expectedOutput: [[3, 3], [3, 4], [3, 5]]
+};
+let testCase5 = {
+    name: 'Matrix 5x5 find 3 circles in left top diagonal',
+    input: [[1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]],
+    expectedOutput: [[1, 1], [2, 2], [3, 3]]
+};
+
+export let  testSuiteCircle = [testCase1, testCase4, testCase5];
+export let  testSuiteCross = [testCase2, testCase3];
+
