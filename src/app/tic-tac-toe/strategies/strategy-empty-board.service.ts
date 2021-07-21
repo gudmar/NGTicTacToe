3ad60c537c31f1a8c36d4237bfb83077cccceb15
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-07-22 00:17:41 +0200

    pattern seracher refactoring: deleted unused variable, that appeared in many places and looked very important

--- src/app/tic-tac-toe/strategies/strategy-empty-board.service.ts
+++ src/app/tic-tac-toe/strategies/strategy-empty-board.service.ts
@@ -39,128 +39,10 @@ export class StrategyEmptyBoardService {
   }
 
   getPattern(figure: FigureNotEmpty, nrOfElementsInRowToWin: number, boardSlice: string[]): any{
-    // let ownFigures = this.getFigureIndexes(figure, boardSlice);
-    // let oponentFigures = this.getFigureIndexes(<FigureNotEmpty>this.opositeFigure(figure), boardSlice)
-    // if (ownFigures.length > 0) return this.getEmptyPattern();
-    // if (oponentFigures.length > 1) return this.getEmptyPattern();
-    // if (oponentFigures.length == 0) return this.getNextMoveProposal(boardSlice);
-    // if (oponentFigures.length == 1) return this.getNextMoveProposals(boardSlice);
     return {
       ownFigures: this.countOwnFigures(figure, boardSlice),
       oponentFigures: this.countOponentFigures(figure, boardSlice),
       isMiddlePositionFree: this.isMiddlePositionOfSliceFree(boardSlice)
-    }
-  // return this.getEmptyPattern()
-  // return {
-  //   foundElements: [],
-  //   nextMoveProposals: [],
-  // }
-  }
-
-  // getPatternForEmptyBoard(figure: FigureNotEmpty, boardModel: string[]): PatternDescriptor {
-  //   let boardSize = this.getBoardSize(boardModel);
-  //   let opositeFigure = function (figure: FigureNotEmpty) { return figure == "Circle" ? "Cross" : "Circle" }
-  //   let nrOfOwnFigureCounter = function () {
-  //     let output = 0;
-  //     for (let item of boardModel) {
-  //       if (figure == item) output++;
-  //     }
-  //     return output;
-  //   }
-  //   let nrOfOponentFigureCounter = function () {
-  //     let output = 0;
-  //     for (let item of boardModel) {
-  //       if (opositeFigure(figure) == item) output++;
-  //     }
-  //     return output;
-  //   }.bind(this)
-  //   if (nrOfOwnFigureCounter() > 0) return this.getEmptyPattern();
-  //   if (nrOfOponentFigureCounter() > 1) return this.getEmptyPattern();
-  //   if (this.isMiddlePositionFree(boardModel)) {
-  //     return {
-  //       foundElements: [],
-  //       nextMoveProposals: [[Math.floor(boardSize / 2), Math.floor(boardSize / 2)]],
-  //     }
-  //   }
-  //   return {
-  //     foundElements: [],
-  //     nextMoveProposals: [[Math.floor(boardSize / 2) - 1, Math.floor(boardSize / 2) + 1]],    
-  //   }
-  // }
-
-  // isMiddlePositionFree(boardModel: string[]) {
-  //   let boardSize = this.getBoardSize(boardModel);
-  //   let midX = Math.floor(boardSize / 2);
-  //   let midY = midX;
-  //   if (boardModel[this.xy2Index(midX, midY, boardSize)] == "") return true;
-  //   return false;
-  // }
-
-  // getBoardSize(boardModel: string[]) {
-  //   return Math.sqrt(boardModel.length + 1)
-  // }
-
-  // xy2Index(x: number, y: number, boardSize: number) {
-  //   return boardSize * y + x - 1;
-  // }
-
-  // xFromIndex(index: number, boardSize: number): number {
-  //   return (index + 1) % boardSize;
-  // }
-
-  // yFromIndex(index: number, boardSize: number): number {
-  //   return Math.floor((index + 1) / boardSize);
-  // }
-
-
-
-
-
-  // getNextMoveProposal(boardSlice: string[]):SlicedPatternDescriptor{
-  //   return {
-  //     foundElements: [],
-  //     nextMoveProposals: [Math.floor((boardSlice.length + 1) / 2)]
-  //   }
-  // }
-
-  // getNextMoveProposals(boardSlice: string[]):SlicedPatternDescriptor{
-  //   let getMiddleOfBoardIndex = Math.floor((boardSlice.length + 1) / 2);
-  //   return {
-  //     foundElements: [],
-  //     nextMoveProposals: [getMiddleOfBoardIndex - 1, getMiddleOfBoardIndex + 1]
-  //   }
-  // }
-
-  // getFigureIndexes(figure: FigureNotEmpty, boardSlice: string[]){
-  //   let figureIndexes: number[] = [];
-  //   let callback = function(element: string, index: number){
-  //     if (element == figure) figureIndexes.push(index);
-  //   }
-  //   boardSlice.forEach(callback)
-  //   return figureIndexes
-  // }
-
-  // countOwnFigures(figure: FigureNotEmpty, boardDescriptor: CellDescriptor[]) {
-  //   return this.countFigures(figure, boardDescriptor)
-  // }
-
-  // countOponentFigures(figure: FigureNotEmpty, boardDescriptor: CellDescriptor[]) {
-  //   return this.countFigures(<FigureNotEmpty>this.opositeFigure(figure), boardDescriptor)
-  // }
-
-  // countFigures(figure: FigureNotEmpty, boardDescriptor: CellDescriptor[]) {
-  //   let nrOfFigures = 0;
-  //   let figureCounter = function (item: CellDescriptor, index: number) {
-  //     if (item.figure == figure) nrOfFigures++
-  //   }
-  //   boardDescriptor.forEach(figureCounter);
-  //   return nrOfFigures;
-  // }
-
-  getEmptyPattern(): SlicedPatternDescriptor {
-    return {
-      foundElements: [],
-      nextMoveProposals: [],
     }
   }
 
@@ -169,6 +51,4 @@ export class StrategyEmptyBoardService {
     if (figure == "Cross") return "Circle";
     return "";
   }
-
-
 }
