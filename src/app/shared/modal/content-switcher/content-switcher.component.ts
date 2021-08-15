ec75ff38c4ca1ac5101deafa3b1aa41bf439b274
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-08-15 22:45:41 +0200

    Change pages inside modal implementatnio: button component implemented

---
+++ src/app/shared/modal/content-switcher/content-switcher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-switcher',
+  templateUrl: './content-switcher.component.html',
+  styleUrls: ['./content-switcher.component.css']
+})
+export class ContentSwitcherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
